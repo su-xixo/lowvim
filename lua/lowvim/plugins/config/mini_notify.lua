68d9837e7c84f57626fae0b667242ad78a919830
Suraj <suraj.vishwakarma.01@yandex.com>
2025-01-25 16:46:29 +0530

    mini notification panel setup

--- lua/lowvim/plugins/config/mini_notify.lua
+++ lua/lowvim/plugins/config/mini_notify.lua
@@ -1,10 +1,48 @@
 local mini_notify = require("mini.notify")
+
+-- custom message format
+local custom_mssg_format = function(notif)
+  local time = vim.fn.strftime('%H:%M', math.floor(notif.ts_update))
+  return string.format('%s (%s)', notif.msg, time)
+end
+
+-- for showing notifications in bottom right corner
+local win_config = function()
+    local has_statusline = vim.o.laststatus > 0
+    local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+    return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
+  end
+
 local M = {}
 local options = {
+     -- Content management
+    content = {
+      format = custom_mssg_format,
+      sort = nil,
+    },
+
+    -- Notifications about LSP progress
+    lsp_progress = {
+      enable = true,
+      duration_last = 1000,
+    },
+
+    -- Window options
+    window = {
+      config = win_config,
+      max_width_share = 0.382,
+      winblend = 25,
+    },
 
 }
 local keymaps = function()
-
+    local prefix = "Notification"
+    lowvim.map('n', '<leader>ns', function()
+        mini_notify.show_history()
+    end, {desc = prefix .. "show"}) -- show notification
+    lowvim.map('n', '<leader>nc', function()
+        mini_notify.clear()
+    end, {desc = prefix .. "clear"}) -- clear notification
 end
 M.setup = function()
     mini_notify.setup(options)
--- lua/lowvim/plugins/ui.lua
+++ lua/lowvim/plugins/ui.lua
@@ -22,6 +22,9 @@ return {
         -- mini.nvim
         'echasnovski/mini.notify',
          version = '*',
+         init = function()
+            vim.notify = require('mini.notify').make_notify()
+        end,
          config = function()
             lowvim.load_config("mini_notify").setup()
          end,
